7a3ffa5fb2a2418dff74a3e5ba6cc26da590e7ad
janickkiy <janickiy@mail.ru>
2024-10-01 17:08:54 +0300

    add files to repository

---
+++ src/helpers.php
@@ -0,0 +1,70 @@
+<?php
+
+if (! function_exists('link_to')) {
+    /**
+     * Generate a HTML link.
+     *
+     * @param string $url
+     * @param string $title
+     * @param array  $attributes
+     * @param bool   $secure
+     * @param bool   $escape
+     *
+     * @return \Illuminate\Support\HtmlString
+     */
+    function link_to($url, ?string $title = null, array $attributes = [], $secure = null, bool $escape = true)
+    {
+        return app('html')->link($url, $title, $attributes, $secure, $escape);
+    }
+}
+
+if (! function_exists('link_to_asset')) {
+    /**
+     * Generate a HTML link to an asset.
+     *
+     * @param string $url
+     * @param string $title
+     * @param array  $attributes
+     * @param bool   $secure
+     *
+     * @return \Illuminate\Support\HtmlString
+     */
+    function link_to_asset(?string $url, ?string $title = null, array $attributes = [], $secure = null)
+    {
+        return app('html')->linkAsset($url, $title, $attributes, $secure);
+    }
+}
+
+if (! function_exists('link_to_route')) {
+    /**
+     * Generate a HTML link to a named route.
+     *
+     * @param string $name
+     * @param string $title
+     * @param array  $parameters
+     * @param array  $attributes
+     *
+     * @return \Illuminate\Support\HtmlString
+     */
+    function link_to_route(string $name, ?string $title = null, array $parameters = [], array $attributes = [])
+    {
+        return app('html')->linkRoute($name, $title, $parameters, $attributes);
+    }
+}
+
+if (! function_exists('link_to_action')) {
+    /**
+     * Generate a HTML link to a controller action.
+     *
+     * @param string $action
+     * @param string $title
+     * @param array  $parameters
+     * @param array  $attributes
+     *
+     * @return \Illuminate\Support\HtmlString
+     */
+    function link_to_action(string $action, ?string $title = null, array $parameters = [], array $attributes = [])
+    {
+        return app('html')->linkAction($action, $title, $parameters, $attributes);
+    }
+}
